737f71a30fd4ab9900098e746a0d986a5c3cd983
fbado66 <fbado66@gmail.com>
2020-10-06 10:53:05 -0400

    Done.

---
+++ build/precache-manifest.9249884cdb146ad127c29ca34b91674b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8f2a92b037d2eb14bd3",
+    "url": "/static/css/main.8a671ebb.chunk.css"
+  },
+  {
+    "revision": "f8f2a92b037d2eb14bd3",
+    "url": "/static/js/main.5931591c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "2848c4939592108500f88b688b9f0128",
+    "url": "/index.html"
+  }
+];
