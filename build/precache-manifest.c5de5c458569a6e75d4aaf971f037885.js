c64dcf2c215f4ed38c8f3740bbabe11118860602
ChristyTropila <tropila04@gmail.com>
2020-10-06 19:31:40 -0400

    Done.

---
+++ build/precache-manifest.c5de5c458569a6e75d4aaf971f037885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b8d341ffd80c67fda1b",
+    "url": "/static/css/main.56dfca4f.chunk.css"
+  },
+  {
+    "revision": "5b8d341ffd80c67fda1b",
+    "url": "/static/js/main.ef7f963d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "92c1bf9d23d0b1ab1980cb1fdd1708cf",
+    "url": "/index.html"
+  }
+];
